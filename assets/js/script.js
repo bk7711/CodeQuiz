4d3fba6ac530ebf3bd16bf2250708f0893c3c997
Carolyn B <bmathematics@gmail.com>
2022-01-29 11:30:59 -0500

    change styling of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,12 @@ var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var correctEl = document.querySelector("#correct");
 var incorrectEl = document.querySelector("#incorrect");
-var scoreEl = document.querySelector("#score");
+var scoreEl = document.querySelector("#scoreKeeper");
 var resultEl = document.querySelector("#result");
 var highscoresEl = document.querySelector(".highscores");
 var displayHighScoresEl = document.querySelector(".displayHighScores");
 var highScores = []
+var currentScore = {};
 
 
 var currentQuestion = 0;
@@ -25,47 +26,59 @@ var scoreEl = 0;
 
 //start timer
 startEl.addEventListener("click", function(){
+    
     saveHighScore();
     var timerInterval = setInterval(function (){
         secondsLeft--;
         // timeEl.innerHTML = secondsLeft;
         document.getElementById("timer").innerHTML = secondsLeft;
+        //stop clock if game is over or time runs out
         if(secondsLeft <= 0 || currentQuestion === bank.length){
             storeScore();
             clearInterval(timerInterval);
         }
 
     }, 1000);
-    startEl.setAttribute("style", "display:none");
+//run the function to pull the question     
     getQuestion();
+//get rid of the start button
+    startEl.setAttribute("style", "display:none");
+    // scoreEl.setAttribute("style", "display:block");
 });
 
 //post a question
 function getQuestion(){
     var q = bank[currentQuestion];
-    resultEl.innerHTML = " ";
     
+    //post the answer choices and create click events for each answer as long as there are questions left
+
     if(currentQuestion < bank.length){
     questionsEl.innerHTML = "<p>" + q.question + "<p>";
+    // resultEl.innerHTML = " ";
     choiceA.innerHTML = q.answers.a;
+    choiceA.setAttribute("style", "border:3px solid blue; border-radius: 10px; background-color:rgba(231,229,229,0.863);")
     choiceA.addEventListener("click",choice);
     choiceB.innerHTML = q.answers.b;
+    choiceB.setAttribute("style", "border:3px solid blue; border-radius: 10px; background-color:rgba(231,229,229,0.863);")
     choiceB.addEventListener("click", choice);
     choiceC.innerHTML = q.answers.c;
+    choiceC.setAttribute("style", "border:3px solid blue; border-radius: 10px; background-color:rgba(231,229,229,0.863);")
     choiceC.addEventListener("click", choice);
     choiceD.innerHTML = q.answers.d;
+    choiceD.setAttribute("style", "border:3px solid blue; border-radius: 10px; background-color:rgba(231,229,229,0.863);")
     choiceD.addEventListener("click", choice);
     }
-    // else{
-    //     storeScore();
-    // }     
+      
 }
-
+//check the if the answer is correct or incorrect and change score if correct or incorrect
 function choice(event){
     if(event.target.id == bank[currentQuestion].correctAnswer){
         correctEl++;
-        currentQuestion++;
+        //inform user of correct answer
         resultEl.innerHTML = "Correct";
+        resultEl.setAttribute("style", "color:green;");
+        currentQuestion++;
+        
 
         console.log("correctEl",correctEl)
         getQuestion();
@@ -73,24 +86,22 @@ function choice(event){
     }else if(event.target.id !== bank[currentQuestion].correctAnswer){
         secondsLeft-=3;
         incorrectEl++;
+        //inform user of inccorrect answer
         resultEl.innerHTML = "Wrong";
+        resultEl.setAttribute("style", "color:red;");
         currentQuestion++;
         console.log("IncorrectEl",incorrectEl)
         getQuestion();
+        
     }
-    // else if(currentQuestion > bank.length){
-    //     currentScore.player = timesUp();
-    //     currentScore.points = correctEl
-    // }
         
 };
-    
+ //once time runs out or last question is answered   
 function endGame(){
-    
     var initials = prompt(`Game over. You scored ${correctEl} points. Enter your initials.`);
     return initials;
 }
-
+//grab the final score and initials and push to highScores array and local storage
 function storeScore(){
     var currentScore = {};
     currentScore.player=endGame().trim();
@@ -98,11 +109,12 @@ function storeScore(){
     highScores.push(currentScore);
     localStorage.setItem("user", JSON.stringify(highScores));
   }
- function saveHighScore(){
+//pull saved scores from localStorage
+function saveHighScore(){
      highScores = JSON.parse(localStorage.getItem('user'));
     console.log(highScores);
  }
-
+//provide info from local storage as the highscores list
 highscoresEl.addEventListener("click",function(){
     saveHighScore();
     for(i = 0; i < highScores.length; i++){
